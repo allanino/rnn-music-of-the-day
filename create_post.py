604f3b2a474352c7ba5ef5960eb3f9a27a0fc8a6
Allan Costa <allaninocencio@yahoo.com.br>
2015-08-14 00:16:58 -0300

    Using named arguments.

--- create_post.py
+++ create_post.py
@@ -12,7 +12,13 @@ def get_link(client, folder_path, opus_number):
     url = url[:-1] + '1'
     return url
 
-def add_post(blog_root, date, opus_number, mp3_url):
+def add_post(client, blog_root, date, opus_number, mp3_path, svg_path):
+    mp3_url = get_link(client, mp3_path, opus_number)
+
+    img = open(os.path.join(blog_root, 'img', '%d.svg' % opus_number), 'wb')
+    with client.get_file(svg_path + '/%d.svg' % opus_number) as f:
+        img.write(f.read())
+
     filename = date + '-opus-%d.md' % opus_number
     post_path = os.path.join(blog_root, '_posts', filename)
     with open(post_path, 'wb') as f:
@@ -22,26 +28,17 @@ def add_post(blog_root, date, opus_number, mp3_url):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('access_token', help="Dropbox access token.")
-    parser.add_argument('mp3_path', help="Dropbox path to MP3 folder.")
-    parser.add_argument('svg_path', help="Dropbox path to SVG folder.")
-    parser.add_argument('opus_number', type=int, help="Opus number n. Used to get n.mp3 and n.svg.")
-    parser.add_argument('blog_root', help="Directory to write the new post")
-    parser.add_argument('--date', help="Date to use on the blog post.")
+    parser.add_argument('--access-token', '-a', help="Dropbox access token.")
+    parser.add_argument('--mp3-path', '-m', help="Dropbox path to MP3 folder.")
+    parser.add_argument('--svg-path', '-s', help="Dropbox path to SVG folder.")
+    parser.add_argument('--opus-number', '-n', type=int, help="Opus number n. Used to get n.mp3 and n.svg.")
+    parser.add_argument('--blog-root', '-b', default='.', help="Directory to write the new post")
+    parser.add_argument('--date', '-d', default=strftime("%Y-%m-%d", gmtime()), help="Date to use on the blog post.")
 
     args = parser.parse_args()
 
     client = dropbox.client.DropboxClient(args.access_token)
 
-    mp3_url = get_link(client, args.mp3_path, args.opus_number)
 
-    img = open(os.path.join(args.blog_root, 'img', '%d.svg' % args.opus_number), 'wb')
-    with client.get_file(args.svg_path + '/%d.svg' % args.opus_number) as f:
-        img.write(f.read())
 
-    if args.date:
-        date = args.date
-    else:
-        date = strftime("%Y-%m-%d", gmtime())
-
-    add_post(args.blog_root, date, args.opus_number, mp3_url)
+    add_post(client, args.blog_root, args.date, args.opus_number, args.mp3_path, args.svg_path)
