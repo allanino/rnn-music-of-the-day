5a00b14f8f14d42c192a9be5ae8c15553f4898ec
Allan Costa <allaninocencio@yahoo.com.br>
2015-12-21 12:12:18 -0200

    Added Opus 126-135.

--- create_post.py
+++ create_post.py
@@ -3,6 +3,10 @@ import argparse
 import dropbox
 import os
 from time import gmtime, strftime
+import datetime
+
+ANCHOR_OPUS = 132
+ANCHOR_DATE = datetime.datetime(2015,12,18,12,0,0)
 
 def get_link(client, folder_path, opus_number):
     folder_metadata = client.metadata(folder_path)
@@ -13,6 +17,7 @@ def get_link(client, folder_path, opus_number):
     return url
 
 def add_post(client, blog_root, date, opus_number, mp3_path, svg_path):
+    date = date.strftime('%Y-%m-%d') 
     mp3_url = get_link(client, mp3_path, opus_number)
 
     img = open(os.path.join(blog_root, 'img', '%d.svg' % opus_number), 'wb')
@@ -31,14 +36,10 @@ if __name__ == '__main__':
     parser.add_argument('--access-token', '-a', help="Dropbox access token.")
     parser.add_argument('--mp3-path', '-m', help="Dropbox path to MP3 folder.")
     parser.add_argument('--svg-path', '-s', help="Dropbox path to SVG folder.")
-    parser.add_argument('--opus-number', '-n', type=int, help="Opus number n. Used to get n.mp3 and n.svg.")
+    parser.add_argument('--opus-number', '-n', type=int, help="Opus number n. Used to get n.mp3 and n.svg and to set the blog date.")
     parser.add_argument('--blog-root', '-b', default='.', help="Directory to write the new post")
-    parser.add_argument('--date', '-d', default=strftime("%Y-%m-%d", gmtime()), help="Date to use on the blog post.")
-
     args = parser.parse_args()
 
+    date = ANCHOR_DATE + datetime.timedelta(days=args.opus_number-ANCHOR_OPUS)
     client = dropbox.client.DropboxClient(args.access_token)
-
-
-
-    add_post(client, args.blog_root, args.date, args.opus_number, args.mp3_path, args.svg_path)
+    add_post(client, args.blog_root, date, args.opus_number, args.mp3_path, args.svg_path)
